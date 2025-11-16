5d2889a16d41ce2f998b01fcc234687aaa9a035e
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-11-17 01:18:49 +0600

    solved 6 problems

--- solution.ts
+++ solution.ts
@@ -1,31 +1,83 @@
-/*
-- This solution.ts file is an example; replace it with your own code.
-- Use the same function names and parameter names as in the problem statement.
-- Write only the code inside the function bodies and return the result.
-- Do not use any console.log statements or comments.
-*/
-
-const getSwallowVelocity = (type: 'african' | 'european'): string => {
-  if (type === 'african') {
-    return 'Roughly 11 meters per second.';
+// Problem 1
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean | undefined {
+  if (typeof value === "string") {
+    return value.toUpperCase();
   }
-  return "I... I don't know that!";
-};
-
-const isCatLiquid = (isAsleep: boolean, containerShape: string): boolean => {
-  return isAsleep && containerShape !== 'none';
-};
-
-class Wizard {
-  name: string;
-  favoriteSpell: string;
-
-  constructor(name: string, favoriteSpell: string) {
-    this.name = name;
-    this.favoriteSpell = favoriteSpell;
+  if (typeof value === "number") {
+    return value * 10;
   }
-
-  castSpell(): string {
-    return `${this.name} casts ${this.favoriteSpell}!`;
+  if (typeof value === "boolean") {
+    return !value;
   }
 }
+
+// problem 2
+function getLength(value: string | any[]): number | undefined {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+}
+
+// problem 3
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `"Name: ${this.name}, Age: ${this.age}"`;
+  }
+}
+
+// problem 4
+type item = { title: string; rating: number };
+function filterByRating(items: item[]): item[] {
+  return items.filter((item) => item.rating >= 4 && item.rating <= 5);
+}
+
+// problem 5
+type user = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: user[]): user[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+// problem 6
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+function printBookDetails(book: Book): void {
+  const bookDetails = `Title: ${book.title}, Author: ${
+    book.author
+  }, Published: ${book.publishedYear}, Available: ${
+    book.isAvailable ? "Yes" : "No"
+  }`;
+  console.log(bookDetails);
+}
+
+// problem 7
+// function getUniqueValues(array1: number[], array2: number[]) {
+//   const uniqueItems = array1.filter((item) => !array2.includes(item));
+//   return [...uniqueItems, ...array2];
+// }
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7];
+// console.log(getUniqueValues(array1, array2));
