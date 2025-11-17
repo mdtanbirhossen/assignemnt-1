e04bad8caea9e9cd4f7ff8f6a271e96ee977e7ed
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-11-17 15:08:17 +0600

    all problems are solved

--- solution.ts
+++ solution.ts
@@ -1,7 +1,7 @@
 // Problem 1
 function formatValue(
   value: string | number | boolean
-): string | number | boolean | undefined {
+): string | number | boolean {
   if (typeof value === "string") {
     return value.toUpperCase();
   }
@@ -11,16 +11,19 @@ function formatValue(
   if (typeof value === "boolean") {
     return !value;
   }
+  throw new Error("Input should be string, number or boolean");
 }
 
 // problem 2
-function getLength(value: string | any[]): number | undefined {
+function getLength(value: string | any[]): number {
   if (typeof value === "string") {
     return value.length;
   }
   if (Array.isArray(value)) {
     return value.length;
   }
+
+  throw new Error("Input should be string or a array");
 }
 
 // problem 3
@@ -34,14 +37,20 @@ class Person {
   }
 
   getDetails(): string {
-    return `"Name: ${this.name}, Age: ${this.age}"`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
 // problem 4
 type item = { title: string; rating: number };
 function filterByRating(items: item[]): item[] {
-  return items.filter((item) => item.rating >= 4 && item.rating <= 5);
+  return items.filter((item) => {
+    if (item.rating < 0 || item.rating > 5)
+      throw new Error("The rating should between 0 to 5");
+    if (item.rating >= 4 && item.rating <= 5) {
+      return true;
+    }
+  });
 }
 
 // problem 5
@@ -73,11 +82,60 @@ function printBookDetails(book: Book): void {
 }
 
 // problem 7
-// function getUniqueValues(array1: number[], array2: number[]) {
-//   const uniqueItems = array1.filter((item) => !array2.includes(item));
-//   return [...uniqueItems, ...array2];
-// }
 
-// const array1 = [1, 2, 3, 4, 5];
-// const array2 = [3, 4, 5, 6, 7];
-// console.log(getUniqueValues(array1, array2));
+function getUniqueValues(
+  array1: (number | string)[],
+  array2: (number | string)[]
+): (number | string)[] {
+  const uniqueArray: (number | string)[] = [];
+  for (let item of array1) {
+    let isExist = false;
+    for (let newItem of uniqueArray) {
+      if (newItem === item) {
+        isExist = true;
+        break;
+      }
+    }
+    if (!isExist) {
+      uniqueArray.push(item);
+    }
+  }
+
+  for (let item of array2) {
+    let isExist = false;
+    for (let newItem of uniqueArray) {
+      if (newItem === item) {
+        isExist = true;
+        break;
+      }
+    }
+    if (!isExist) {
+      uniqueArray.push(item);
+    }
+  }
+  return uniqueArray;
+}
+
+// problem 8
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+function calculateTotalPrice(products: Product[]): number {
+  if (products.length === 0) {
+    return 0;
+  }
+  let total = 0;
+  products.map(
+    (product) =>
+      (total =
+        total +
+        (product.discount
+          ? product.price - (product.price * product.discount) / 100
+          : product.price) *
+          product.quantity)
+  );
+  return total;
+}
